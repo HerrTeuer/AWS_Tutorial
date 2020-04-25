51919810ae9cf292ec9e99688e9e83d98fed186d
herrteuer <guiyuxin1992@gmail.com>
2020-04-25 10:58:52 +0200

    get and list object in s3 bucket

--- pratice/s3/readjson.py
+++ pratice/s3/readjson.py
@@ -3,9 +3,22 @@ import boto3
 
 s3 = boto3.resource("s3")
 bucket = "mytempfileforcloudwatchmetrics"
-key="2020-04-16/EC2-CPUUtilization-i-08be9e7184ce17f36-2020-04-16.csv"
+key="configuration/test/config.json"
 
 response = s3.Object(bucket,key).get()
 body=response["Body"].read()
-print(body)
-jsonfile = json.load(body)
+jsonfile = json.loads(body)
+#print(jsonfile)
+
+s3_client = boto3.client("s3")
+response = s3_client.list_objects_v2(
+    Bucket= bucket,
+    Prefix="2020-04-16/",
+)
+#print(response)
+contents = response["Contents"]
+filelist = []
+for content in contents:
+    filelist.append(content["Key"])
+
+print(filelist)
